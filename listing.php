89b1f62bcebb33c10fe6e08426b1c12d7d2c5260
Serena Chen <ohhelloserena@gmail.com>
2016-08-22 21:07:57 -0700

    Update listing.php

--- listing.php
+++ listing.php
@@ -1,24 +1,31 @@
 <?php 
 
-$servername = "127.0.0.1:3306";
-$username = "root";
-$password = "";
+//$servername = "127.0.0.1:3306";
+//$username = "root";
+//$password = "";
+
+// ClearDB MySQL
+$DB_HOST = "us-cdbr-iron-east-04.cleardb.net";
+$DB_DATABASE = "heroku_e7a6e415d917761";
+$DB_USERNAME = "b6b2ab681e26b0";
+$DB_PASSWORD = "4a7a9103";
 
 // Connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+//$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM restaurant.menu WHERE category='Lunch'";
+$sql = "SELECT * WHERE category='Lunch'";
 $result = $conn->query($sql);
 
-$sqlApp = "SELECT * FROM restaurant.menu WHERE category='Appetizer'";
+$sqlApp = "SELECT * WHERE category='Appetizer'";
 $resultApp = $conn->query($sqlApp);
 
-$sqlMain = "SELECT * FROM restaurant.menu WHERE category='Main'";
+$sqlMain = "SELECT * WHERE category='Main'";
 $resultMain = $conn->query($sqlMain);
 
 ?>
@@ -29,26 +36,49 @@ $resultMain = $conn->query($sqlMain);
 <meta charset="utf-8" />
 <title>Connie's Cookhouse</title>
 
+<?php echo "<link rel='style' type='text/css' href='style.css' />" ?>
+
 </head>
+
 <body>
+
+<div id="container">
+	<header>
+		<div id="header-content">
+			<h1><a href="main.html" class="class1">Connie's Cookhouse</a></h1>
+			
+			<nav>
+				<ul>
+					<li><a href="http://www.conniescookhouse.com/menu.pdf" class="class1">Menu</a></li>
+					<li><a href="#Bot" class="class1">Location + Hours</a></li>
+					<li><a href="localhost/restaurant/listing.php" class="class1">Order Online</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	
+	<div id="banner">
+		<h2><span>Healthy Chinese Cooking</span></h2>
+	</div>
+
 
 <div id="content">
 <article>
 
 		<!-- Appetizer drop down-->
-		<p>
-		<select>
-		<option value"">Appetizers</option>
-		<?php 
-		if ($resultApp->num_rows > 0) {
-			while($row = $resultApp->fetch_assoc()) { ?>
-				<option value="<?php echo $row['name']; ?>"><?php echo $row['name']; ?> - $<?php echo $row['price']?></option>
-				<br>
-				<?php
-			}
-			} ?>
-		</select>
-		</p>
+			<p>
+			<select>
+			<option value"">Appetizers</option>
+			<?php 
+			if ($resultApp->num_rows > 0) {
+				while($row = $resultApp->fetch_assoc()) { ?>
+					<option value="<?php echo $row['name']; ?>"><?php echo $row['name']; ?> - $<?php echo $row['price']?></option>
+					<br>
+					<?php
+				}
+				} ?>
+			</select>
+			</p>
 
 		<!-- Lunch drop down-->
 		<p>
@@ -79,12 +109,9 @@ $resultMain = $conn->query($sqlMain);
 			} ?>
 		</select>
 		</p>
-		
 		<p><button type="button" onclick="alert('Your order has been submitted.')">Submit</button></p>
-
 </article>
 </div>
 
 </body>
 </html>
-
